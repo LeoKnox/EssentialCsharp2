920c23ec38f22faab67169cc866be2a622b86507
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-30 20:52:11 -0700

    Create Array01.cs

---
+++ Arrays/Array01.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string[] groceryList = new string[4];
+		groceryList[0] = "Steak";
+		groceryList[2] = "Cheese";
+		groceryList[1] = "Potato";
+		groceryList[3] = "Salad";
+	}
+}
