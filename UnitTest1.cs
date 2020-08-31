ab2ec66ae297e64d820bbedf4975aa67c85b9f5a
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-30 20:06:24 -0700

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testInstance = new Class1();
+            var testResult = testInstance.AddTwo(5, 5);
+            Assert.AreEqual(10, testResult, "AddTwo 5 + 5 = 10");
+        }
+    }
+}
