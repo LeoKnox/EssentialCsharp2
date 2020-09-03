21c6ba34f084a78272a07cc0976e1520f6eee714
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-02 20:43:23 -0700

    Update practice1.cs

--- tests/practice1.cs
+++ tests/practice1.cs
@@ -5,6 +5,15 @@ public class Program
 	public class Character
 	{
 		public string Name { get; set; }
+		public string Class { get; set; }
+		public string Race { get; set; }
+		public int Stat1 { get; set; }
+		public int Stat2 { get; set; }
+		public int Stat3 { get; set; }
+		public int Hp { get; set; }
+		public int Def { get; set; }
+		public int Atk { get; set; }
+		
 	}
 	public static void Main()
 	{
