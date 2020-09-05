1f7f25b1635f1938edef2ce19bc17136305aa810
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-04 19:07:52 -0700

    Add files via upload

--- Class1.cs
+++ Class1.cs
@@ -6,6 +6,10 @@ namespace EssentialTraining
     {
         public int AddTwo(int a, int b)
         {
+            var groceryList = new string[4] { "Eveehi", "Elric", "Conan", "Sonya" };
+            Array.Resize(ref groceryList, 6);
+            groceryList[4] = "Kord";
+            groceryList[5] = "Aelien";
             return a + b;
         }
     }
