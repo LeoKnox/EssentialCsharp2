8a8a52e3b12d26bfd77decef393935e61611b213
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-04 20:02:56 -0700

    Add files via upload

---
+++ SimpleArrayTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class SimpleArrayTest
+    {
+        [TestMethod]
+        public void TestInstantiation()
+        {
+            var testInstance = new SimpleArray();
+            Assert.AreEqual(testInstance.AdventurerList.Length, 4);
+            Assert.AreEqual(testInstance.AdventurerList[1], "Conan");
+        }
+        [TestMethod]
+        public void TestToString()
+        {
+            var testInstance = new SimpleArray();
+            Assert.IsTrue(testInstance.ToString().StartsWith("There are"));
+        }
+    }
+}
