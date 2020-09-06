69070c5ad0546854fe22600da7da3c2515351694
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-05 21:10:36 -0700

    Add files via upload

--- SimpleArray.cs
+++ SimpleArray.cs
@@ -21,10 +21,6 @@ namespace EssentialTraining
                 {4,5,6,7 },
                 {8,9,10,11 }
             };
-            var CharClasses = new List<string>();
-            CharClasses.Add("Fighter");
-            CharClasses.Add("Wizard");
-            CharClasses.Sort();
         }
 
         public override string ToString()
