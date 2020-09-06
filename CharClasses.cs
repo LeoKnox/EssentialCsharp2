b63f3d58b68b22c4ae7696cd078fc8052793b7a3
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-05 21:10:47 -0700

    Add files via upload

---
+++ CharClasses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining
+{
+    public class CharClasses
+    {
+        public List<string> CharacterClasses { get; set; }
+        public CharClasses()
+        {
+            CharacterClasses = new List<string>();
+        }
+        public bool DoesClassExist(string myclass)
+        {
+            return CharacterClasses.Contains(myclass);
+        }
+    }
+}
