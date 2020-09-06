437d005795474a5740f4f6166a8b5776579997fa
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-05 21:22:54 -0700

    Add files via upload

---
+++ CharClassesTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class CharClassesTest
+    {
+        [TestMethod]
+        public void IsClassTest()
+        {
+            var testInstance = new CharClasses();
+            testInstance.CharacterClasses.Add("Fighter");
+            testInstance.CharacterClasses.Add("Wizard");
+            testInstance.CharacterClasses.Add("Rogue");
+
+            // expect to pass
+            Assert.IsTrue(testInstance.DoesClassExist("Rogue"));
+            // expect to fail
+            Assert.IsFalse(testInstance.DoesClassExist("Sloth"));
+        }
+    }
+}
