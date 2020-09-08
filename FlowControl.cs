5c0cb002978ef812c1903383bf9debe4cd337f8f
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-07 18:45:33 -0700

    Add files via upload

---
+++ FlowControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining
+{
+    public class FlowControl
+    {
+        public bool IsClassWizard(string charClass)
+        {
+            if (charClass.ToLower() == "wizard")
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
