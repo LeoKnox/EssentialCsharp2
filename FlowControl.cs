7915b039656569642217e191f84b8fbc79e32463
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-07 19:32:44 -0700

    Add files via upload

--- FlowControl.cs
+++ FlowControl.cs
@@ -6,9 +6,23 @@ namespace EssentialTraining
 {
     public class FlowControl
     {
+        public string MagicOrMundane(string charClass)
+        {
+            var result = "";
+            switch(charClass.ToLower())
+            {
+                case "wizard":
+                    result = "Magic";
+                    break;
+                default:
+                    result = "Mundane";
+                    break;
+            }
+            return result;
+        }
         public string ClassOrSubclass(string charClass)
         {
-            var result = "":
+            var result = "";
             if (charClass.ToLower() == "Fighter")
             {
                 result = "Class";
