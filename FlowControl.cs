392bdbb20a570c6fa01a411402d9f2bfdd3180a0
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-07 19:00:03 -0700

    Add files via upload

--- FlowControl.cs
+++ FlowControl.cs
@@ -6,6 +6,35 @@ namespace EssentialTraining
 {
     public class FlowControl
     {
+        public string ClassOrSubclass(string charClass)
+        {
+            var result = "":
+            if (charClass.ToLower() == "Fighter")
+            {
+                result = "Class";
+            }
+            else if (charClass.ToLower() == "Rogue")
+            {
+                result = "Class";
+            }
+            else if (charClass.ToLower() == "Wizard")
+            {
+                result = "Class";
+            }
+            else
+            {
+                result = "SubClass";
+            }
+            return result;
+        }
+        public bool IsClassFighter(string charClass)
+        {
+            return (charClass.ToLower() == "fighter");
+        }
+        public bool IsClassRogue(string charClass)
+        {
+            return (charClass.ToLower() == "rogue") ? true : false;
+        }
         public bool IsClassWizard(string charClass)
         {
             if (charClass.ToLower() == "wizard")
