1caf815a097b72e0d9d2f87e4985edf398c85f45
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-08 22:18:54 -0700

    Add files via upload

---
+++ ProgrammingLoops.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining
+{
+    class ProgrammingLoops
+    {
+        public void ForLoop()
+        {
+            int sum = 0;
+            for(int i = 0; i < 100; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
