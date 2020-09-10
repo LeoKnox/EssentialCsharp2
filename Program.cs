85d2365eebd8d54da834dc8e5c3dc84e0a2202e8
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-09 21:32:50 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EssentialTrainingApp
+{
+    class Program
+    {
+        public static List<string> Words;
+        static void Main(string[] args)
+        {
+            Words.Add("Steak");
+            Words.Add("Potato");
+            Words.Add("Wine");
+        }
+    }
+}
